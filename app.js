2247da3c55ad3ccdd246390c9d5f5a8eb3481d5b
Helio Franco <dev.francohel@gmail.com>
2021-11-09 19:17:54 -0300

    FEAT: criando rotas /usuarios /quadro /atividade

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
+const port = 3000
 
+app.get('/usuario', (req, res) => {
+    res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
+  })
+
+app.get('/atividade', (req, res) => {
+    res.send('Rota ativada com GET e recurso atividade: valores de atividade devem ser retornados')
+  })
+
+app.get('/quadro', (req, res) => {
+    res.send('Rota ativada com GET e recurso quadro: valores de quadro devem ser retornados')
+  })  
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })  
